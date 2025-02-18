23bbede0629d1ec1fdeda9950fdfbb10b5580632
SterneStehen <7353718@gmail.com>
2025-02-19 00:13:56 +0100

    add pair.cpp

--- stady/pair.cpp
+++ stady/pair.cpp
@@ -9,6 +9,5 @@ int main()
 	std::pair<int, std::string> p2(1, "alon");
 
 	std::cout << p1.first << "   " << p1.second << std::endl;
-	std::cout << p2.first << "   " << p2.second << std::endl;
-
+	std::cout << p2.first << "   " << p2.second << std::end
 }
