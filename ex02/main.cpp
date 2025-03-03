c2ca700da6cb148159e5acc362804c50a9a641a4
SterneStehen <7353718@gmail.com>
2025-03-04 00:06:45 +0100

    add time to programm

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smoreron <smoreron@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 20:25:22 by smoreron          #+#    #+#             */
-/*   Updated: 2025/03/02 22:51:12 by smoreron         ###   ########.fr       */
+/*   Updated: 2025/03/04 00:06:25 by smoreron         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -15,7 +15,6 @@
 #include <list>
 #include <ctime> 
 #include "PmergeMe.hpp"
-
 
 int  ft_atoi(char *str){
 	int res;
@@ -33,7 +32,7 @@ bool isPositiv(char *str){
 	int i = 0;
 	while (str[i] != '\0')
 	{
-		if(str[i] < '0' || str[i] > '9')
+		if(str[i] < '0')
 			return 0;
 		i++;
 	}
@@ -53,7 +52,7 @@ int main(int ac, char *av[]){
 	for (int i = 1; i < ac; ++i)
 	{
 		if(!isPositiv(av[i])){
-			std::cout << "Not positiv number" << std::endl;
+			std::cout << "Error: Not positiv number" << std::endl;
 			return 0;
 		}
 		tmp = ft_atoi(av[i]);
@@ -77,10 +76,36 @@ int main(int ac, char *av[]){
 	}
 
 	PmergeMe PM;
-	PM.megreSortVector(vect);
-	std::cout << "\nNEW VECTOR: " << std::endl;
-	for(auto it = vect.begin(); it != vect.end(); it++){
-		std::cout << *it <<  " ";
-	}
+	PmergeMe PM;
 
+	try {
+	clock_t startVect = clock();
+	PM.megreSortVector(vect);
+	clock_t endVect = clock();
+	double timeVect = static_cast<double>(endVect - startVect) / CLOCKS_PER_SEC * 1'000'000.0;
+
+	clock_t startList = clock();
+	PM.megreSortList(list);
+	clock_t endList = clock();
+	double timeList = static_cast<double>(endList - startList) / CLOCKS_PER_SEC * 1'000'000.0;
+
+	std::cout << "\nNEW VECTOR: " << std::endl;
+	for (std::vector<int>::iterator it = vect.begin(); it != vect.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "LIST: " << std::endl;
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "Time to process a range of " << vect.size() << " elements with std::vector : " << timeVect << " us" << std::endl;
+	std::cout << "Time to process a range of " << list.size() << " elements with std::list  : " << timeList << " us" << std::endl;
+	}
+	catch (const std::exception &e) {
+		std::cerr << e.what() << '\n';
+	return 1;
+	}
 }
